ab2a14f7dcccb94e7ac82095f658ab8f6d320242
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-02-13 01:00:12 +0100

    Update nodown version and add TableOfContent component

--- src/components/table-of-content/TableOfContent.tsx
+++ src/components/table-of-content/TableOfContent.tsx
@@ -1,42 +1,69 @@
-import { Anchor } from "antd";
+// Définissez vos interfaces ici
+interface TocElement {
+  type: string;
+  children: TocElement[];
+  href?: string;
+}
 
-function TableOfContent() {
+interface AnchorItem {
+  key: string;
+  href: string;
+  title: string;
+  children?: AnchorItem[];
+}
+
+function convertData(data: TocElement): AnchorItem[] {
+  return data.children.map((element: TocElement, index: number): AnchorItem => {
+    const test = element.children.find(
+      (child: TocElement) => child.type === "link"
+    );
+
+    if (!test || !test.href) {
+      throw new Error("Link element or href not found");
+    }
+
+    const title = renderToHTML(test);
+    const titleElement = document.createElement("span");
+    titleElement.innerHTML = title;
+    const item: AnchorItem = {
+      key: `${index + 1}`,
+      href: test.href,
+      title: titleElement.textContent || "",
+    };
+
+    if (
+      element.children?.some(
+        (child: TocElement) => child.type === "unordered-list"
+      )
+    ) {
+      const list = element.children.find(
+        (child: TocElement) => child.type === "unordered-list"
+      );
+      if (list) {
+        item.children = convertData(list);
+      }
+    }
+    return item;
+  });
+}
+
+import { Anchor } from "antd";
+import { renderToHTML } from "nodown";
+
+function TableOfContent({
+  toc,
+}: {
+  toc?: { type: string; children: TocElement[] };
+}) {
+  if (!toc) return null;
+
+  const convertedData = convertData(toc);
+
   return (
     <aside>
       <div>
         <header>Summary</header>
-        <Anchor
-          affix={false}
-          items={[
-            {
-              key: "1",
-              href: "#components-anchor-demo-basic",
-              title: "Basic demo",
-            },
-            {
-              key: "2",
-              href: "#components-anchor-demo-static",
-              title: "Static demo",
-            },
-            {
-              key: "3",
-              href: "#api",
-              title: "API",
-              children: [
-                {
-                  key: "4",
-                  href: "#anchor-props",
-                  title: "Anchor Props",
-                },
-                {
-                  key: "5",
-                  href: "#link-props",
-                  title: "Link Props",
-                },
-              ],
-            },
-          ]}
-        />
+        <Anchor affix={false} items={convertedData} />
       </div>
     </aside>
   );
